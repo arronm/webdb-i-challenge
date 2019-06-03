888f326474e98113664f3278882dec0871fd68ee
Arron M <marshall.arron@gmail.com>
2019-06-03 10:22:43 -0700

    Set up endpoint scaffold

--- server.js
+++ server.js
@@ -2,6 +2,38 @@ const express = require('express');
 
 const server = express();
 
-// your code here
+// GET
+server.get('/api/accounts', async (req, res) => {
+  res.json({
+    message: 'GET Endpoint',
+  });
+});
+
+server.get('/api/accounts/:id', async (req, res) => {
+  res.json({
+    message: `GET Endpoint for ID: ${req.params.id}`,
+  });
+});
+
+// POST
+server.post('/api/accounts', async (req, res) => {
+  res.json({
+    message: 'POST Endpoint',
+  });
+});
+
+// PUT
+server.put('/api/accounts/:id', async (req, res) => {
+  res.json({
+    message: `PUT Endpoint for ID: ${req.params.id}`,
+  });
+});
+
+// DELETE
+server.delete('/api/accounts/:id', async (req, res) => {
+  res.json({
+    message: `DELETE Endpoint for ID: ${req.params.id}`,
+  });
+});
 
 module.exports = server;
